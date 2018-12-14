5ebab765267c492996a808339b488e2f52b8136f
Ammar <ammar.alnatsheh@gmail.com>
2018-12-14 15:28:19 -0800

    implement the database

--- database/index.js
+++ database/index.js
@@ -1,16 +1,42 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost:27017/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  name: String,
+  updated: { type: Date, default: Date.now() }
 });
 
-let Repo = mongoose.model('Repo', repoSchema);
+let Repo = mongoose.model('fetcher', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (repos, callback) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  
+  repos.forEach(record => {
+    // apply an insert query to each repo 
+    Repo.create({ name: record }, function (err) {
+
+      if (err)  callback(err);
+    });
+    callback(null);
+
+  });
 }
 
-module.exports.save = save;
+let get = (callback) => {
+  // TODO: Your code here
+  // This function should return the most recent 25
+  // record accourding to its update
+  // the MongoDB
+
+  Repo.
+  find().
+  sort({ updated: -1 }).
+  limit(25).
+  select('name').
+  exec(callback); // where callback is the name of our callback function.
+
+}
+
+module.exports = {save,get};
