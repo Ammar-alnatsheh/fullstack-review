4f935949f83c6942a98b559d68f6aff26e6451ee
Ammar <ammar.alnatsheh@gmail.com>
2018-12-15 15:21:15 -0800

    have the database name and url have '' as default value

--- database/index.js
+++ database/index.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/fetcher');
 
 let repoSchema = mongoose.Schema({
-  name: String,
-  html_url: String,
+  name: { type: String, default: '' },
+  html_url: { type: String, default: '' },
   updated: { type: Date, default: Date.now() }
 });
 
@@ -36,7 +36,7 @@ let get = (callback) => {
 
   Repo.
   find().
-  select('name').
+  select('name','html_url').
   sort({ updated: -1 }).
   limit(25).
   exec(callback); // where callback is the name of our callback function.
